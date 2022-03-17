b1c0788454f3c8b462a0e88dc51fe1ec2a924f76
Kyle <kyle@kxnrl.com>
2022-03-17 19:18:31 +0800

    Validate config

--- __CI/validator.sh
+++ __CI/validator.sh
@@ -3,6 +3,8 @@
 #KeyValues
 echo "Download KeyValues Validator ..."
 wget "https://github.com/Kxnrl/php-utility/raw/master/KeyValues/KeyValues.php" -q -O KeyValues.php
-wget "https://github.com/Kxnrl/php-utility/raw/master/KeyValues/vdf.php" -q -O vdf.php
+echo "Download Configs Validator ..."
+wget "https://github.com/Kxnrl/php-utility/raw/master/Configs/Configs.php" -q -O Configs.php
 
 php -f KeyValues.php
+php -f Configs.php
